979f36865a4503b6711048e61df6f75930228d9d
prdn <paolo.ardoino@gmail.com>
2017-04-18 21:37:20 +0000

    code cleanup

--- utils.js
+++ utils.js
@@ -18,47 +18,18 @@ var get_conf_json = (env, type, path) => {
   return res
 }
 
-var str_to_bin = (s) => {
-  return new Buffer(s).toString('binary')
-} 
-
-const IV = '1234567890123456'
-
-var crypto_key_parse = (key) => {
-  return key.substr(0, 32)
-}
-
-var crypto_decrypt = (cryptkey, _iv, encryptdata) => {
-  if (!encryptdata) return ''
-
-  const iv = _iv || IV
-  encryptdata = new Buffer(encryptdata, 'base64').toString('binary')
-
-  const decipher = crypto.createDecipheriv('aes-256-cbc', crypto_key_parse(cryptkey), iv)
-  let decoded  = decipher.update(encryptdata, 'binary', 'utf8')
-
-  decoded += decipher.final('utf8')
-  return decoded
-}
-
-var crypto_encrypt = (cryptkey, _iv, cleardata) => {
-  if (!cleardata) return ''
-
-  const iv = _iv || IV
-  const encipher = crypto.createCipheriv('aes-256-cbc', crypto_key_parse(cryptkey), iv)
-  let encryptdata  = encipher.update(cleardata, 'utf8', 'binary')
-
-  encryptdata += encipher.final('binary')
-  return new Buffer(encryptdata, 'binary').toString('base64')
-}
-
 function ring_id(val, dim) {
   return val && dim ? val % dim : 0
 }
 
+function elapsed(t) {
+  var precision = 3
+  var elapsed = process.hrtime(t)[1] / 1000000; // divide by a million to get nano to milli
+  return process.hrtime(t)[0] + "s," + elapsed.toFixed(precision) + "ms"
+}
+
 module.exports = {
   ring_id : ring_id,
-  decrypt: crypto_decrypt,
-  encrypt: crypto_encrypt,
-  get_conf_json: get_conf_json
+  get_conf_json: get_conf_json,
+  elapsed: elapsed
 }
