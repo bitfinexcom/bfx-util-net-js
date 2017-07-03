fe54d12d0b23dcd1859e2061df414363e6e621a2
prdn <paolo.ardoino@gmail.com>
2017-07-03 16:08:46 +0000

    mc

---
+++ workers/base1.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const Base0 = require('./base0')
+
+class Base1 extends Base0 {
+
+  addFac (type, name, ns, label, opts, cb) {
+    opts.dirConf = `${__dirname}/../config/facs`
+    super.addFac(type, name, ns, label, opts, cb)
+  }
+}
+
+module.exports = Base1
