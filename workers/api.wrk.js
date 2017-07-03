b3dfd5c013646750c992dbb15232b7c9a3bc88ff
prdn <paolo.ardoino@gmail.com>
2017-07-03 16:41:24 +0000

    base api class

---
+++ workers/api.wrk.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const async = require('async')
+const Base = require('./base')
+
+class WrkApi extends Base {
+
+  init () {
+    super.init()
+
+    this.conf.init.facilities.push(
+      ['fac', 'grc', 'p0', 'bfx', {}],
+      ['fac', 'grc', 'p0', 'bfx_api', {
+        services: ['rest:coin:admin'],
+        svc_port: this.conf.net.util.apiBfxPort
+      }],
+      ['fac', 'api', 'bfx', 'bfx_util', {
+        path: this.conf.bfx_api_class
+      }]
+    )
+  }
+
+  getPluginCtx (type) {
+    const ctx = super.getPluginCtx(type)
+
+    switch (type) {
+      case 'api_bfx_util':
+        ctx.grc_bfx = this.grc_bfx
+        break
+    }
+
+    return ctx
+  }
+
+  _start (cb) {
+    async.series([ next => { super._start(next) },
+      next => {
+        if (this.api_bfx_util) {
+          this.grc_bfx_api.set('api', this.api_bfx_util.api)
+        }
+
+        next()
+      }
+    ], cb)
+  }
+}
+
+module.exports = WrkApi
