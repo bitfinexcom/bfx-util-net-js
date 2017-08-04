657daaafcd8df1be6ed615e3e38d7c178185272b
prdn <paolo.ardoino@gmail.com>
2017-08-04 14:57:31 +0000

    updates

--- workers/api.wrk.js
+++ workers/api.wrk.js
@@ -9,14 +9,16 @@ class WrkApi extends Base {
     super.init()
 
     this.conf.init.facilities.push(
-      ['fac', 'grc', 'p0', 'bfx', {}],
-      ['fac', 'grc', 'p0', 'bfx_api', this.getApiGrcConf()],
-      ['fac', 'api', 'bfx', 'bfx_main', this.getApiConf()]
+      ['fac', 'grc', 'p0', 'bfx', this.getGrcConf()],
+      ['fac', 'api', 'bfx', 'bfx', this.getApiConf()]
     )
   }
 
-  getApiGrcConf () {
-    return { svc_port: 0, services: [] }
+  getGrcConf () {
+    return { 
+      svc_port: this.ctx.apiPort || 0,
+      services: []
+    }
   }
 
   getApiConf () {
@@ -29,7 +31,7 @@ class WrkApi extends Base {
     const ctx = super.getPluginCtx(type)
 
     switch (type) {
-      case 'api_bfx_main':
+      case 'api_bfx':
         ctx.grc_bfx = this.grc_bfx
         break
     }
@@ -40,8 +42,8 @@ class WrkApi extends Base {
   _start (cb) {
     async.series([ next => { super._start(next) },
       next => {
-        if (this.api_bfx_main) {
-          this.grc_bfx_api.set('api', this.api_bfx_main.api)
+        if (this.api_bfx) {
+          this.grc_bfx.set('api', this.api_bfx.api)
         }
 
         next()
