902b747981fb13a4a44289d29e4030d8cff9e1a8
prdn <paolo.ardoino@gmail.com>
2017-10-25 13:53:17 +0000

    mc

---
+++ workers/api.net.util.wrk.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const { WrkApi } = require('bfx-wrk-api')
+const maxmind = require('maxmind')
+const path = require('path')
+const fs = require('fs')
+
+const newDb = path.join(__dirname, '..', 'mmdb', 'GeoLite2-ASN.mmdb')
+
+class WrkUtilNetApi extends WrkApi {
+  constructor (conf, ctx) {
+    super(conf, ctx)
+
+    this.loadConf('net.util', 'util')
+
+    this.init()
+    this.start()
+  }
+
+  getGrcConf () {
+    const grcConf = super.getGrcConf()
+    grcConf.services = ['rest:util:net']
+
+    return grcConf
+  }
+
+  getApiConf () {
+    return {
+      path: 'net.util'
+    }
+  }
+
+  getPluginCtx (type) {
+    super.init()
+
+    const ctx = super.getPluginCtx(type)
+
+    switch (type) {
+      case 'api_bfx':
+        ctx.asn_db = this.asnDb
+        break
+    }
+
+    return ctx
+  }
+
+  init () {
+    super.init()
+
+    this.testIfDbExists()
+    this.asnDb = maxmind.openSync(newDb, { watchForUpdates: true })
+  }
+
+  testIfDbExists () {
+    if (!fs.existsSync(newDb)) {
+      throw new Error('GEO_DB_NOT_INSTALLED - run `npm run update-asn-data`')
+    }
+  }
+}
+
+module.exports = WrkUtilNetApi
