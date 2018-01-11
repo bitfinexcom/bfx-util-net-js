af0bcbc730f2c6411d87b95bb6aa2874fcfbd6fe
Davide Scola <davide@bitfinex.com>
2018-01-11 16:20:01 +0000

    update the ASN database only if it is newer than the local copy

--- update-asn-db.sh
+++ update-asn-db.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz -O ./mmdb/GeoLite2-ASN.tar.gz
-cd mmdb && tar -zxvf GeoLite2-ASN.tar.gz --strip 1
+DESTDIR='./mmdb'
+ME="${BASH_SOURCE[0]}"
+TS="$(date +'%s' --reference="${DESTDIR}/GeoLite2-ASN.tar.gz" 2>/dev/null)"
+
+wget --quiet --timestamping --directory-prefix="${DESTDIR}" 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz' || {
+  echo "${ME##*/}: fatal error: download failed." >&2
+  exit 1
+}
+
+[[ "${TS}" = "$(date +'%s' --reference="${DESTDIR}/GeoLite2-ASN.tar.gz" 2>/dev/null)" ]] || {
+  tar --extract --directory "${DESTDIR}" --strip-components=1 \
+    --wildcards '*.mmdb' --file "${DESTDIR}/GeoLite2-ASN.tar.gz" || exit 1
+}
+
+exit 0
