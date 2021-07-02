295d5fa25e3ed15aacd4b23d473fd65f29622248
vigan <vigan.abd@gmail.com>
2021-07-02 11:14:17 +0200

    updated docs and scripts

--- update-asn-db.sh
+++ update-asn-db.sh
@@ -2,16 +2,15 @@
 
 DESTDIR='./mmdb'
 ME="${BASH_SOURCE[0]}"
-TS="$(date +'%s' --reference="${DESTDIR}/GeoLite2-ASN.tar.gz" 2>/dev/null)"
 
-wget --quiet --timestamping --directory-prefix="${DESTDIR}" 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz' || {
+wget --quiet --output-document="${DESTDIR}/GeoLite2-ASN.tar.gz" 'https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN&license_key=YOUR_LICENSE_KEY&suffix=tar.gz' || {
   echo "${ME##*/}: fatal error: download failed." >&2
   exit 1
 }
 
-[[ "${TS}" = "$(date +'%s' --reference="${DESTDIR}/GeoLite2-ASN.tar.gz" 2>/dev/null)" ]] || {
-  tar --extract --directory "${DESTDIR}" --strip-components=1 \
-    --wildcards '*.mmdb' --file "${DESTDIR}/GeoLite2-ASN.tar.gz" || exit 1
-}
+tar --extract --directory "${DESTDIR}" --strip-components=1 \
+  --wildcards '*.mmdb' --file "${DESTDIR}/GeoLite2-ASN.tar.gz" || exit 1
+
+rm "${DESTDIR}/GeoLite2-ASN.tar.gz"
 
 exit 0
