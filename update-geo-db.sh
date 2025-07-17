040c2c7b323e30c0eede65cac97811d4741f8e69
Amin Abdul Manaf <afolanyaaminu@gmail.com>
2025-07-17 09:37:19 +0000

    update update scripts to look for maxmind license in the right place and make wget make noise when update-asn script fals

--- update-geo-db.sh
+++ update-geo-db.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ -z ${MAXMIND_LICENSE+x} ]
-then
-  echo "Error, MAXMIND_LICENSE env must be set"
+if [ -z "$MAXMIND_KEY" ]; then
+  echo "Error: MAXMIND_KEY environment variable must be set"
+  echo "Usage: export MAXMIND_KEY=license_key"
+  exit 1
 fi
 
-env LICENSE_KEY="$MAXMIND_LICENSE" node ./scripts/updatedb.js
+LICENSE_KEY="$MAXMIND_KEY" node ./scripts/updatedb.js
 
-exit 0
+
